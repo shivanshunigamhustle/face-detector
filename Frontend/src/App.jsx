8ca7776a1a28785bf2c689cf5685585919a2119d
shivanshunigam <shivanshunigam6@gmail.com>
2026-03-03 12:51:10 +0530

    moodify complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import FaceExpression from './features/Expression/components/FaceExpression'
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes"
+import "./features/shared/styles/global.scss"
+import { AuthProvider } from "./features/auth/auth.context"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <FaceExpression/>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,16 @@
-const {Router} = require('express');
-const authController = require('../controller/auth.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
+const { Router } = require("express");
+const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 const router = Router();
 
-router.post('/register', authController.registerUser);
 
-router.post('/login', authController.loginUser);
+router.post('/register', authController.registerUser)
 
-router.get("/get-me", authMiddleware.authUser, authController.getMe);
-router.get("/logout",authController.logoutUser);
+router.post('/login', authController.loginUser)
+
+router.get("/get-me", authMiddleware.authUser, authController.getMe)
+
+router.get("/logout", authController.logoutUser)
+
+
 module.exports = router;
